7691fb8293f925ca636a4124e64acf30d9778144
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-05-30 16:17:07 +0500

    update

--- houseView.php
+++ houseView.php
@@ -136,7 +136,7 @@ updateHouse();
                             </div>
                         </form>
                         <!-- -------------Print----------- -->
-                        <div class="content-wrapper" id="printable-content">
+                        <div class="content-wrapper" hidden id="printable-content">
                             <link rel="preconnect" href="https://fonts.googleapis.com">
                             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
                             <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
