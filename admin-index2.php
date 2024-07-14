02ed14f5b7caad3a1c1f3d843d3c8a9e66b68d4e
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-07-15 00:21:09 +0500

    update

--- admin-index2.php
+++ admin-index2.php
@@ -201,7 +201,7 @@ function filter_maintenace_data_In_Database($maintenaiceLimited, $maintenaceOrde
                     </a>
                 </td>
                 <td>
-                <a href="includes/pdf_maker?MAT_ID='. $row['maintenance_id'] .'&ACTION=VIEW" >  <span style="padding: 5px 1px; border-radius: 5px; color: white; background-color:lightcoral;">
+                <a href="includes/pdf_maker?MAT_ID='. $row['maintenance_id'] .'&ACTION=VIEW" target="_blank" >  <span style="padding: 5px 1px; border-radius: 5px; color: white; background-color:lightcoral;">
                         <i class="fas fa-file text-white p-1">Print</i>
                         </span></a>  
                 </td>
