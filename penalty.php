ef3fdfa566f4d21409e502985c015a19143922c9
Abu Hammad <hammadkamal2003@gmail.com>
2024-08-12 21:14:00 +0500

    hammad

--- penalty.php
+++ penalty.php
@@ -93,7 +93,7 @@ include ("includes/sidebar.php");
                                 </select>
                             </div>
                             <div class="me-2">
-                                <a class="d2c_pdf_btn text-center justify-content-center text-decoration-none text-black"
+                                <a class="d2c_pdf_btn text-center justify-content-center text-decoration-none text-primary"
                                     href="">
                                     <span><i class="fas fa-file-pdf mt-2"></i></span>
                                 </a>
